e28a42e5c76457337704cfe6eef81dd37f939f6e
Alisha Alisha <aalisha@quantium.com.au.local>
2016-06-02 15:56:09 +0530

    WPF

---
+++ WPF/WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //    //// Create the StackPanel 
+        //    //StackPanel stackPanel = new StackPanel();
+        //    //this.Content = stackPanel;
+
+        //    //// Create the Button 
+        //    //Button button = new Button();
+        //    //button.Content = "Click Me";
+        //    //button.HorizontalAlignment = HorizontalAlignment.Left;
+        //    //button.Margin = new Thickness(150);
+        //    //button.VerticalAlignment = VerticalAlignment.Top;
+        //    //button.Width = 75;
+        //    //stackPanel.Children.Add(button);  
+
+          //pnlMainGrid.MouseUp += new MouseButtonEventHandler(pnlMainGrid_MouseUp);            
+        //}
+
+        //private void pnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
+        //}
+
+        private void StaticAndDynamicResources_Loaded(object sender, RoutedEventArgs e)
+        {
+            stkPanel.Resources["buttonBackground"] = Brushes.Yellow;
+        }
+    }
+}
